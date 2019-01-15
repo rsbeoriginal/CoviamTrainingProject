0284057d063c33dd82652219537acb665d7dd686
rishi <rishisharma7361@gmail.com>
2019-01-15 20:05:46 +0530

    JPA lib added

--- CollegeManagement/src/main/java/com/example/CollegeManagement/entity/Department.java
+++ CollegeManagement/src/main/java/com/example/CollegeManagement/entity/Department.java
@@ -1,4 +1,27 @@
 package com.example.CollegeManagement.entity;
 
+import org.springframework.data.annotation.Id;
+
+
 public class Department {
+
+    @Id
+    String departmentId;
+    String departmentName;
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getDepartmentName() {
+        return departmentName;
+    }
+
+    public void setDepartmentName(String departmentName) {
+        this.departmentName = departmentName;
+    }
 }
