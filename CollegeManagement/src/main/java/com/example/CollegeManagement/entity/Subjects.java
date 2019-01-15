2d09fed56278a096bb6fa14a0305500c8ae94ef9
rohanmarwaha <rohanmarwaha222@gmail.com>
2019-01-15 20:15:34 +0530

    Subjects entity initialised.

--- CollegeManagement/src/main/java/com/example/CollegeManagement/entity/Subjects.java
+++ CollegeManagement/src/main/java/com/example/CollegeManagement/entity/Subjects.java
@@ -1,6 +1,68 @@
 package com.example.CollegeManagement.entity;
 
+import com.sun.javafx.beans.IDProperty;
+import org.hibernate.annotations.GenericGenerator;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name=Subjects.TABLE_NAME)
 public class Subjects {
 
+    public static final String TABLE_NAME="SUBJECTS";
 
+
+
+    @Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name="uuid",strategy = "uuid2")
+    public String subjectId;
+    public String subjectName;
+    public String departmentId;
+    public String professorId;
+
+    public String getSubjectId() {
+        return subjectId;
+    }
+
+    public void setSubjectId(String subjectId) {
+        this.subjectId = subjectId;
+    }
+
+    public String getSubjectName() {
+        return subjectName;
+    }
+
+    public void setSubjectName(String subjectName) {
+        this.subjectName = subjectName;
+    }
+
+    public String getDepartmentId() {
+        return departmentId;
+    }
+
+    public void setDepartmentId(String departmentId) {
+        this.departmentId = departmentId;
+    }
+
+    public String getProfessorId() {
+        return professorId;
+    }
+
+    public void setProfessorId(String professorId) {
+        this.professorId = professorId;
+    }
+
+    @Override
+    public String toString() {
+        return "Subjects{" +
+                "subjectId='" + subjectId + '\'' +
+                ", subjectName='" + subjectName + '\'' +
+                ", departmentId='" + departmentId + '\'' +
+                ", professorId='" + professorId + '\'' +
+                '}';
+    }
 }
